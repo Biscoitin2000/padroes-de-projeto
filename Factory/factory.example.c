02b7a5b50071c17e8c4d2b66982bf87ca558dae7
TaldoVitinho <vitormoraes2486@gmail.com>
2024-12-05 08:30:40 -0300

    Update factory.example.c
    Implementa o comportamento de envio de uma mensagem SMS.

--- Factory/factory.example.c
+++ Factory/factory.example.c
@@ -7,3 +7,11 @@
 typedef struct Notification {
     void (*send)(struct Notification*, const char* message);
 } Notification;
+
+typedef struct SMSNotification {
+    Notification base;
+} SMSNotification;
+
+void sendSMS(Notification* self, const char* message) {
+    printf("Enviando SMS: %s\n", message);
+}
